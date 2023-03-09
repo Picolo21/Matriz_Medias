0f4b2cf4fcef20eee6af6588814c7d07e26e0a46
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-03-09 10:21:17 -0300

    Alterações feitas em aula melhorando o código

--- Matriz_Medias/Program.cs
+++ Matriz_Medias/Program.cs
@@ -11,9 +11,9 @@
         do
         {
             Console.Clear();
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < mat.GetLength(0); i++)
             {
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < (mat.GetLength(1) - 1); j++)
                 {
                     Console.Write($"Digite um valor na posição mat[{i}, {j}] = ");
                     mat[i, j] = double.Parse(Console.ReadLine());
@@ -21,14 +21,14 @@
             }
             Console.WriteLine();
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < mat.GetLength(0); i++)
             {
                 mat[i, 2] = (mat[i, 0] + mat[i, 1]) / 2;
             }
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < mat.GetLength(0); i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < mat.GetLength(1); j++)
                 {
                     Console.Write(mat[i, j].ToString("F2").PadRight(7) + " ");
                 }
